db5581bdf0ac6044b7670969a7a474009aa87d8d
thando <tzulu49@gmail.com>
2023-12-17 16:17:50 +0200

    Created Sentence Service & Added DB Configurations

---
+++ sentence-service/src/main/java/com/runninghill/sentenceservice/SentenceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.runninghill.sentenceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentenceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentenceServiceApplication.class, args);
+	}
+
+}
---
+++ sentence-service/src/test/java/com/runninghill/sentenceservice/SentenceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runninghill.sentenceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentenceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
